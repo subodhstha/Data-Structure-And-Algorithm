7458edab60cbf188fba38af639fb484083180089
subodhstha <shresthasubodh73@gmail.com>
2022-12-04 15:48:24 +0545

    change

--- Lab 7 (Searching and Hashing)/1-linear_search.c
+++ Lab 7 (Searching and Hashing)/1-linear_search.c
@@ -29,7 +29,8 @@ int main()
 
 int LinearSearch(int *arr, int n,int key)
 {
-    for(int i=0;i<n; i++) {
+    for(int i=0;i<n; i++)
+    {
         if(arr[i]== key)
         {
             return i+1;
